dd133b8d5bc4f863834d8564ee7b805e382ddd97
saikurelli <sairajareddykurelli@gmail.com>
2023-02-23 09:42:36 -0600

    Added Base Xcode Proj

---
+++ Habitochi/Habitochi/ViewController.swift
@@ -0,0 +1,18 @@
+// Project: Habitochi
+//  
+//  EID: sk49777
+//  Course: CS371L
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
