4d0feb3c3e72836a618dc06a2c0c61a99ae35e86
Vincent Cacciatore <vincentcacciatore@vincents-mbp-4.lan>
2023-03-02 19:16:41 -0600

    habit creation implementations and alerts

--- Habitochi/Habitotchi/HabitCreationViewController.swift
+++ Habitochi/Habitotchi/HabitCreationViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class HabitCreationViewController: UIViewController {
 
+    @IBOutlet weak var habitNameField: UITextField!
+    @IBOutlet weak var habitDescriptionField: UITextField!
+    @IBOutlet weak var daysOfTheWeekStack: UIStackView!
+    @IBOutlet weak var notificationTimeField: UIDatePicker!
+    
+    @IBOutlet weak var sundayButton: UIButton!
+    @IBOutlet weak var mondayButton: UIButton!
+    @IBOutlet weak var tuesdayButton: UIButton!
+    @IBOutlet weak var wednesdayButton: UIButton!
+    @IBOutlet weak var thursdayButton: UIButton!
+    @IBOutlet weak var fridayButton: UIButton!
+    @IBOutlet weak var saturdayButton: UIButton!
+    
     private let createAccountSegueIdentifier = "accountCreateHabitSegueIdentifier"
     var sender = ""
     override func viewDidLoad() {
@@ -19,6 +32,71 @@ class HabitCreationViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         configureNavBar()
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        // title for alert when user misinputs habit
+        let errorTitle = "Missing Field"
+        
+        // get the name of habit, cannot be empty
+        let habitNameEntered = habitNameField.text
+        if habitNameEntered == nil {
+            let controller = UIAlertController(title: errorTitle, message: "Habit Name Field empty", preferredStyle: .alert)
+            controller.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            present(controller, animated: true)
+        }
+        
+        // get habit description
+        let habitDescriptionEntered = habitDescriptionField.text
+        
+        // get days of the week selected from buttons
+        let daysOfTheWeekSelected = getDaysOfTheWeek()
+        if daysOfTheWeekSelected.count == 0{
+            let controller = UIAlertController(title: errorTitle, message: "No days selected", preferredStyle: .alert)
+            controller.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            present(controller, animated: true)
+            
+        }
+        
+        // get time selected from time wheel
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat =  "HH:mm a"
+        let timeEntered = dateFormatter.string(from: notificationTimeField.date)
+        
+        let habitCreated = Habit(name: habitNameEntered!, goal: habitDescriptionEntered!)
+        
+        
+    }
+    
+    // helper methods of reading which buttons were selected
+    func getDaysOfTheWeek() -> [String]{
+        let daysOfWeek = Calendar.current.weekdaySymbols
+        var enteredDaysOfWeek = [String]()
+        if sundayButton.isSelected{
+            enteredDaysOfWeek.append(daysOfWeek[0])
+        }
+        if mondayButton.isSelected{
+            enteredDaysOfWeek.append(daysOfWeek[1])
+        }
+        if tuesdayButton.isSelected{
+            enteredDaysOfWeek.append(daysOfWeek[2])
+        }
+        if wednesdayButton.isSelected{
+            enteredDaysOfWeek.append(daysOfWeek[3])
+        }
+        if thursdayButton.isSelected{
+            enteredDaysOfWeek.append(daysOfWeek[4])
+        }
+        if fridayButton.isSelected{
+            enteredDaysOfWeek.append(daysOfWeek[5])
+        }
+        if saturdayButton.isSelected{
+            enteredDaysOfWeek.append(daysOfWeek[6])
+        }
+        
+        return enteredDaysOfWeek
+        
     }
     
     private func configureNavBar(){
