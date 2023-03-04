b1313fa366217f69b97fe218c4760b03a0d3ab14
saikurelli <sairajareddykurelli@gmail.com>
2023-03-03 19:40:25 -0600

    Added NavBar to Habit

--- Habitochi/Habitotchi/HabitStatsViewController.swift
+++ Habitochi/Habitotchi/HabitStatsViewController.swift
@@ -26,32 +26,32 @@ class HabitStatsViewController: UIViewController {
         super.viewDidLoad()
         
         // setting up Sprite Animal
-        
         let scene = SKScene(size: animalSprite.bounds.size)
         scene.backgroundColor = UIColor.white
         self.view.bounds = UIScreen.main.bounds
         scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        
-        
         scene.addChild(fetchedHabit.animal.sprite.createSprite(size: animalSprite.bounds.size))
         animalSprite.presentScene(scene)
         
         
-        
+        // set up labels
         habitLabel.text = fetchedHabit.name
         currentStreak.text = String(fetchedHabit.daysCompleted)
         goalLabel.text = fetchedHabit.goal
         longestStreak.text = String(0)
         
+        setUpPicker()
+        
         // Do any additional setup after loading the view.
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        configureNavBar()
+    func setUpPicker(){
+        goalStatViewMode.dataSource
     }
     
-    @IBAction func buttonPressed(_ sender: Any) {
-        fetchedHabit.habitCircleChecked()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureNavBar()
     }
     
     private func configureNavBar(){
@@ -59,5 +59,11 @@ class HabitStatsViewController: UIViewController {
         navController?.navigationBar.backgroundColor = .white
         navController?.navigationBar.tintColor = DARK_GREEN
         navController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:DARK_GREEN]
+        // edit button at nav top right
+        let nav = self.navigationItem
+        let editStatScreen = UIBarButtonItem(title: "profileButton", style: UIBarButtonItem.Style.done, target: self, action: nil)
+        editStatScreen.image = UIImage(systemName: "pencil.and.outline")
+        editStatScreen.tintColor = DARK_GREEN
+        nav.rightBarButtonItem = editStatScreen
         }
 }
